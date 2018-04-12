639e841b68030192b0c4570c27229916ec9d0ba9
Chibisof81 <chibisof81@yandex.ru>
2018-04-12 19:34:41 +0300

    Добавил before и after

--- src/test/java/com/BlazeDemo/tests/FirstTestSelenium.java
+++ src/test/java/com/BlazeDemo/tests/FirstTestSelenium.java
@@ -1,30 +1,43 @@
 package com.BlazeDemo.tests;
 
 
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import static java.lang.System.setProperty;
 import static org.testng.Assert.assertTrue;
 import static org.testng.AssertJUnit.assertEquals;
 
 
 public class FirstTestSelenium {
 
+    WebDriver driver;
+
+    @BeforeMethod
+    public void before() {
+        setProperty("webdriver.chrome.driver", "D:/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
     @Test
     public void Test() {
-        System.setProperty("webdriver.chrome.driver", "D:/chromedriver.exe");
 
-        WebDriver driver = new ChromeDriver();
         driver.get("http://blazedemo.com");
         WebElement FindFlights = driver.findElement(By.cssSelector("input"));
         FindFlights.click();
         WebElement VirginAmerica = driver.findElement(By.cssSelector("tbody tr:nth-of-type(1) td:nth-child(4)"));
         assertTrue(VirginAmerica.isDisplayed());
         assertEquals("ok", "http://blazedemo.com/reserve.php", "http://blazedemo.com/reserve.php");
+    }
+
+    @AfterMethod
+    public void after() {
         driver.quit();
     }
+
 }
