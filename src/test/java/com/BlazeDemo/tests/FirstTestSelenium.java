0948206463a3b54a129bd367d5953fc68b4c316e
Chibisof81 <chibisof81@yandex.ru>
2018-04-22 23:30:31 +0300

    поправил первый тест

--- src/test/java/com/BlazeDemo/tests/FirstTestSelenium.java
+++ src/test/java/com/BlazeDemo/tests/FirstTestSelenium.java
@@ -1,28 +1,13 @@
 package com.BlazeDemo.tests;
 
-
+import com.BlazeDemo.AbstractTest;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import static java.lang.System.setProperty;
 import static org.testng.Assert.assertTrue;
 import static org.testng.AssertJUnit.assertEquals;
 
-
-public class FirstTestSelenium {
-
-    WebDriver driver;
-
-    @BeforeMethod
-    public void before() {
-        setProperty("webdriver.chrome.driver", "D:/chromedriver.exe");
-        driver = new ChromeDriver();
-    }
+public class FirstTestSelenium extends AbstractTest {
 
     @Test
     public void test() {
@@ -31,12 +16,8 @@ public class FirstTestSelenium {
         FindFlights.click();
         WebElement VirginAmerica = driver.findElement(By.cssSelector("tbody tr:nth-of-type(1) td:nth-child(4)"));
         assertTrue(VirginAmerica.isDisplayed());
-        assertEquals("ok", "http://blazedemo.com/reserve.php", "http://blazedemo.com/reserve.php");
-    }
-
-    @AfterMethod
-    public void after() {
-        driver.quit();
+        String page = driver.getCurrentUrl();
+        assertEquals(page, "http://blazedemo.com/reserve.php");
     }
 
 }
